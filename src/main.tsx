8ad8cd8c70a5337f341281f15d01a4d62536cb1d
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-07-21 19:39:59 +0700

    feat: add UI blog detail

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import Homepage from "./pages/Homepage";
 import Loginpage from "./pages/Loginpage";
 import Register from "./pages/Register";
+import BlogDetailpage from "./pages/BlogDetailpage";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Loginpage />,
+  },
+  {
+    path: "/blogs/:id",
+    element: <BlogDetailpage />,
   },
 ]);
 
